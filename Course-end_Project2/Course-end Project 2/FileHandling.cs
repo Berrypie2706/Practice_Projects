9c6261ce03c85489f4c01bae90332ec559b189cc
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-08-03 20:38:22 +0530

    Course End Project

---
+++ Course-end_Project2/Course-end Project 2/FileHandling.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace Course_end_Project_2
+{
+    public class FileHandling
+    {
+        public void Retrieve(string fName)
+        {
+           
+                try
+            {
+                string path = "D:\\Practice Projects\\Course-end Project 2\\";
+                string fPath = path + fName;
+                if(File.Exists(fPath))
+                {
+                    string[] lines = File.ReadAllLines(fPath);
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File Not Exists");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
+        }
+
+        public void Update(string fName,int id, string name, string Class, string section)
+        {
+            try
+            {
+                string path = "D:\\Practice Projects\\Course-end Project 2";
+                string fPath = path + fName;
+                if (File.Exists(fPath))
+                {
+                    StreamWriter streamWriter = File.AppendText(fPath);
+                    Console.WriteLine("\n");
+                    streamWriter.WriteLine( id + "," + name + "," + Class + "," + section + ".");                
+                    streamWriter.Dispose();
+                    streamWriter.Close();
+                    Console.WriteLine(" {0},{1},{2},{3}",id, name, Class, section);              
+                    Console.WriteLine("Data is updated successfully");
+                }
+                else
+                {
+                    Console.WriteLine("File Not Found");
+                }
+
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Format Exception:" +ex.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error while updating the data :" + e.Message);
+            }
+
+        }
+    }
+}
---
+++ Course-end_Project2/Course-end Project 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Course_end_Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                try
+            {
+                FileHandling file = new FileHandling();
+                Console.WriteLine("Select the Operation");
+                Console.WriteLine("1. Retrieve the data \n 2. Update the data ");
+                int op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                            {
+                                Console.WriteLine("Enter File Name");
+                                string fileName = Console.ReadLine();
+
+                                file.Retrieve(fileName);
+                                break;
+                            }
+
+                    case 2:
+                            {
+                                Console.WriteLine("Enter the name of the file where you want to update the data");
+                                string fileName = Console.ReadLine();
+                                Console.WriteLine("Enter Teacher data \n");
+                                Console.Write("ID: \t");
+                                int id = int.Parse(Console.ReadLine());
+                                Console.Write("Name:\t");
+                                string name = Console.ReadLine();
+                                Console.Write("Class:\t");
+                                string Class = Console.ReadLine();
+                                Console.Write("Section: ");
+                                string section = Console.ReadLine();
+                                file.Update(fileName, id, name, Class, section);
+                                break;
+                            }                                                                        
+                    default:
+                            {
+                                Console.WriteLine("File not found");
+                                break;
+                            }
+                      
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Oops there is some thing wrong" + ex.Message);
+            }
+            Console.WriteLine("Press X If You Want To Retry.");
+
+            Console.WriteLine("Press Any Key To Exit!!!!");
+            choice = Console.ReadLine();
+        }
+            while (choice== "x" || choice=="X");
+            Console.ReadKey();
+
+
+        }
+    }
+}
